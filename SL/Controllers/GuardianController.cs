ff9b76e98ea2fa4c2f0dbfde9e783f64957e676f
nube_bala <nube_bala@NUBE>
2018-04-09 18:05:41 +0530

    Nominee & Guardian Saved Not Test

--- SL/Controllers/GuardianController.cs
+++ SL/Controllers/GuardianController.cs
@@ -22,6 +22,30 @@ namespace SL.Controllers
         {
             try
             {
+                DAL.GuardianInsertBranch nm = new DAL.GuardianInsertBranch();
+
+                nm.MEMBER_CODE = wm.MEMBER_CODE;
+                nm.ICNO_OLD = wm.ICNO_OLD;
+                nm.ICNO_NEW = wm.ICNO_NEW;
+                nm.NAME = wm.NAME;
+                nm.SEX = wm.SEX;
+                nm.AGE = wm.AGE;
+                nm.RELATION_CODE = wm.RELATION_CODE;
+                nm.ADDRESS1 = wm.ADDRESS1;
+                nm.ADDRESS2 = wm.ADDRESS2;
+                nm.ADDRESS3 = wm.ADDRESS3;
+                nm.CITY_CODE = wm.CITY_CODE;
+                nm.STATE_CODE = wm.STATE_CODE;
+                nm.COUNTRY = wm.COUNTRY;
+                nm.ZIPCODE = wm.ZIPCODE;
+                nm.PHONE = wm.PHONE;
+                nm.MOBILE = wm.MOBILE;
+                nm.USER_CODE = wm.USER_CODE;
+                nm.ENTRY_DATE = wm.ENTRY_DATE;
+                nm.ENTRY_TIME = wm.ENTRY_TIME;
+
+                db.GuardianInsertBranches.Add(nm);
+                db.SaveChanges();
 
                 return Json(new { isSaved = true }, JsonRequestBehavior.AllowGet);
             }
--- SL/TS/Joinup.ts
+++ SL/TS/Joinup.ts
@@ -9,33 +9,79 @@ import { MASTERRACE } from "./MasterRace";
 import { MASTERCITY } from "./MASTERCITY";
 import { MASTERSTATE } from "./MASTERSTATE";
 import { MASTERCOUNTRY } from "./MasterCountry";
+import { MASTERNOMINEE } from "./MasterNominee";
+import { MASTERGUARDIAN } from "./MasterGuardian";
+import { MASTERRELATION } from "./MasterRelation";
 
-class Joinup{
+class Joinup {
     data: MASTERMEMBER;
+    dataGuardian: MASTERGUARDIAN;
+    nominee: KnockoutObservable<string>;
+    Guardian: KnockoutObservable<string>;
     bankList: MASTERBANK[];
     branchList: MASTERBRANCH[];
     raceList: MASTERRACE[];
     cityList: MASTERCITY[];
     stateList: MASTERSTATE[];
     countryList: MASTERCOUNTRY[];
+    nomineeList: KnockoutObservableArray<MASTERNOMINEE>;
+    relationList: MASTERRELATION[];
 
     constructor() {
         this.data = new MASTERMEMBER();
+        this.nominee = ko.observable<string>("No");
+        this.Guardian = ko.observable<string>("No");
+        
         this.bankList = MASTERBANK.toList();
         this.branchList = MASTERBRANCH.toList();
         this.raceList = MASTERRACE.toList();
         this.cityList = MASTERCITY.toList();
         this.stateList = MASTERSTATE.toList();
-        this.countryList = MASTERCOUNTRY.toList();
+        this.countryList = MASTERCOUNTRY.toList();        
+        this.relationList = MASTERRELATION.toList();
+
+        this.nomineeList = ko.observableArray<MASTERNOMINEE>();
+        this.AddNominee();
     }
 
     btnSave(): void {
         var d = ko.toJS(this.data);
         console.log(d);
-        $.post('./MasterMember/Insert', d, function (r) {
-            console.log(r);
+        $.post('./MasterMember/Insert', d,  (resMember)=> {
+            console.log(resMember);
+            if (resMember.isSaved) {
+                if (this.nominee() == "Yes") {
+                    ko.utils.arrayForEach(this.nomineeList(), (n) => {
+                        n.MEMBER_CODE(resMember.MEMBER_CODE());
+                        var dN = ko.toJS(n);
+                        $.post('./MasterNominee/Insert', dN, (resNominee) => {
+                            
+                        });
+                    });
+                }
+
+                if (this.Guardian() == "Yes") {
+                    this.Guardian.MEMBER_CODE(resMember.MEMBER_CODE());
+                    var dG = ko.toJS(this.dataGuardian);
+                    $.post('./MasterGuardian/Insert', dG, (resGuardian) => {
+
+                    });
+                }
+            }
         });
 
+    }
+
+    AddNominee(): void {
+        this.nomineeList.push(new MASTERNOMINEE());
+    }
+
+    RemoveNominee(data: MASTERNOMINEE): void {
+        if (confirm("Are you remove this nominee?")) {
+            this.nomineeList.remove(x => {
+               return x == data;
+            });
+        }        
     }
 
 }
