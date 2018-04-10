d68bfeb9b3deb971090c598e84fd8d6ccf44081e
nube_bala <nube_bala@NUBE>
2018-04-10 13:15:39 +0530

    ko jui

---
+++ SL/Scripts/JS/koDatePicker.js
@@ -0,0 +1,8 @@
+var koDatePicker = /** @class */ (function () {
+    function koDatePicker() {
+    }
+    koDatePicker.prototype.init = function () {
+    };
+    return koDatePicker;
+}());
+//# sourceMappingURL=koDatePicker.js.map
--- SL/TS/Main.ts
+++ SL/TS/Main.ts
@@ -3,7 +3,8 @@
         "knockout": "knockout-3.4.2",
         "jquery": "jquery-3.0.0.min",
         "jqueryui": "jquery-ui-1.12.1.min",
-        "knockout-jqAutocomplete": "knockout-jqAutocomplete"
+        "knockout-jqAutocomplete": "knockout-jqAutocomplete",
+        "knockout-jqueryui":"knockout-jqueryui"
     },
     baseUrl:'http://localhost/MembershipTest/Scripts/'
 });
