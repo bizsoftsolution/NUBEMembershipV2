78bd79315cbc8cfa3232c055eca652cd8f39c59a
nube_naveen <nube_naveen@NUBE>
2019-03-08 16:14:24 +0800

    History Alter completd

---
+++ Nube/AppLog.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+
+namespace Nube
+{
+    public static class AppLog
+    {
+
+        public static string WriteLogState = "On";
+        public static string WriteLogFileName = "NUBE_log";
+
+        public static void WriteLog(String str)
+        {
+            if (WriteLogState.ToLower() != "off")
+            {
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(Path.GetTempPath() + WriteLogFileName + ".txt", true))
+                    {
+                        writer.WriteLine(str);
+                    }
+                }
+                catch (Exception) { }
+            }
+
+        }
+
+        public static void WriteLog(String str, params object[] args)
+        {
+            if (WriteLogState.ToLower() != "off")
+            {
+                try
+                {
+                    WriteLog(string.Format(str, args));
+                }
+                catch (Exception ex) { WriteLog(ex); }
+            }
+        }
+
+
+        public static void WriteLogDT(String str)
+        {
+            if (WriteLogState.ToLower() != "off")
+            {
+                try
+                {
+                    WriteLog(string.Format("{0:dd/MM/yyyy hh:mm:ss} => {1}", DateTime.Now, str));
+                }
+                catch (Exception) { }
+            }
+
+        }
+
+
+        public static void WriteLogDT(String str, params object[] args)
+        {
+            if (WriteLogState.ToLower() != "off")
+            {
+                try
+                {
+                    WriteLogDT(string.Format(str, args));
+                }
+                catch (Exception ex) { WriteLog(ex); }
+            }
+        }
+
+
+        public static void WriteLog(Exception ex)
+        {
+            WriteLogDT(string.Format("Error=> ExMessage:{0},StackTrace:{1}", ex.Message, ex.StackTrace));
+        }
+
+        public static void DisplayMsg(int Left, int Top, string Msg)
+        {
+            Console.CursorLeft = Left;
+            Console.CursorTop = Top;
+            Console.WriteLine(Msg);
+        }
+        public static string ReadMsg(int Left, int Top)
+        {
+            Console.CursorLeft = Left;
+            Console.CursorTop = Top;
+            return Console.ReadLine();
+        }
+        public static void DisplayClear()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+        }
+    }
+}
--- Nube/frmHistoryAlter.xaml.cs
+++ Nube/frmHistoryAlter.xaml.cs
@@ -85,6 +85,12 @@ namespace Nube
             try
             {
                 mmStatus = new MemberMonthEndStatu();
+
+                mmStatus.MEMBERTYPE_CODE = data.MEMBERTYPE_CODE;
+                mmStatus.BANK_CODE = data.BANK_CODE;
+                mmStatus.BRANCH_CODE = data.BRANCH_CODE;
+                mmStatus.NUBE_BRANCH_CODE = data.MASTERBANKBRANCH.NUBE_BRANCH_CODE;
+
                 this.gbxStatus.DataContext = mmStatus;
             }catch(Exception ex) { }
         }
@@ -103,8 +109,251 @@ namespace Nube
         {
             if (MessageBox.Show("Do you want to Re-Create the History?", "Re-Create", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
             {
-                
+                MemberHistoryClean();
             }
+        }
+
+        void MemberHistoryClean()
+        {
+            try
+            {
+                AppLog.WriteLogFileName = $"MemberHistoryClean_MemberId_${data.MEMBER_ID}";
+
+                DateTime dtFrom = data.MemberMonthEndStatus.Min(x => x.StatusMonth.Value);
+                DateTime dtTo = data.MemberMonthEndStatus.Max(x => x.StatusMonth.Value);
+                DateTime? dtResign = null;
+
+                var resign = data.RESIGNATIONs.FirstOrDefault();                
+                if (resign!=null) dtResign= resign.VOUCHER_DATE.Value;
+
+                int CMon = 0;
+                int PMon = 0;
+                int DMon = 0;
+
+
+                decimal BF = 0;
+                decimal Subs = 0;
+                decimal Ins = 0;
+
+                decimal ConBF = 0;
+                decimal ConSubs = 0;
+                decimal ConIns = 0;
+
+                decimal AccBF = 0;
+                decimal AccSubs = 0;
+                decimal AccIns = 0;
+
+                decimal DueBF = 0;
+                decimal DueSubs = 0;
+                decimal DueIns = 0;
+
+
+                decimal CurBF = 0;
+                decimal CurSubs = 0;
+                decimal CurIns = 0;
+
+                var st = data.MemberMonthEndStatus.FirstOrDefault(x => x.StatusMonth == dtFrom);
+                if (st == null)
+                {
+                    AppLog.WriteLog($"{dtFrom:MMMM yyyy} is missing in History");
+                    return;
+                }
+                if (st.StatusMonth == new DateTime(2005, 9, 1))
+                {
+                    bool hasError = false;
+
+                    PMon = Convert.ToInt32(st.TOTALMONTHSPAID.Value);
+                    DMon = Convert.ToInt32(st.TOTALMONTHSDUE.Value);
+                    CMon = PMon + DMon;
+
+                    var dt = data.DATEOFJOINING.Value;
+                    dt = new DateTime(dt.Year, dt.Month, 1);
+                    var tmpCMon = dt.MonthDiff(dtFrom);
+                    if (CMon != tmpCMon)
+                    {
+                        AppLog.WriteLog($"Contribute Month : {CMon}, Paid Month: {PMon}, Due Month: {DMon}");
+                        AppLog.WriteLog($"Error: Contribute Month is mismatch. Actual Contribute Month is {tmpCMon}");
+                        hasError = true;
+                    }
+                   
+
+                    AccBF = st.ACCBF.Value;
+                    AccSubs = st.ACCSUBSCRIPTION.Value;
+                    AccIns = st.ACCINSURANCE.Value;
+
+                    DueBF = st.BFDUE.Value;
+                    DueSubs = st.SUBSCRIPTIONDUE.Value;
+                    DueIns = st.INSURANCEDUE.Value;
+
+                    AppLog.WriteLog($"AccBF: {AccBF}, AccSubs: {AccSubs}, AccIns: {AccIns}, DueBF: {DueBF}, DueSubs: {DueSubs}, DueIns: {DueIns}");
+
+                    if (AccBF != (PMon * st.BF_AMOUNT))
+                    {
+                        AppLog.WriteLog("AccBF is mismatch");
+                        hasError = true;
+                    }
+
+                    if (DueBF != (PMon * st.BF_AMOUNT))
+                    {
+                        AppLog.WriteLog("DueBF is mismatch");
+                        hasError = true;
+                    }
+
+                    if (hasError) return;
+                }
+                else
+                {
+                    CMon = 1;
+                    PMon = Convert.ToInt16(st.TOTAL_MONTHS.Value);
+                }
+                DMon = CMon - PMon;
+
+                BF = st.BF_AMOUNT.Value;
+                Subs = st.SUBSCRIPTION_AMOUNT.Value;
+                Ins = st.INSURANCE_AMOUNT.Value;
+
+                CurBF = st.CURRENT_YDTBF.Value;
+                CurSubs = st.CURRENT_YDTSUBSCRIPTION.Value;
+                CurIns = st.CURRENT_YDTINSURANCE.Value;
+
+                AccBF = st.ACCBF.Value;
+                AccSubs = st.ACCSUBSCRIPTION.Value;
+                AccIns = st.ACCINSURANCE.Value;
+
+                DueBF = st.BFDUE.Value;
+                DueSubs = st.SUBSCRIPTIONDUE.Value;
+                DueIns = st.INSURANCEDUE.Value;
+
+                ConBF = AccBF + DueBF;
+                ConSubs = AccSubs + DueSubs;
+                ConIns = AccIns + DueIns;
+
+                AppLog.WriteLog($"Contribute Month : {CMon}, Paid Month: {PMon}, Due Month: {DMon}");
+                int LastPaidDue = 0;
+                while (dtFrom < dtTo)
+                {
+
+                    
+                    dtFrom = dtFrom.AddMonths(1);
+
+                    if (dtFrom.Month == 4)
+                    {
+                        CurBF = 0;
+                        CurSubs = 0;
+                        CurIns = 0;
+                    }
+
+                    st = data.MemberMonthEndStatus.FirstOrDefault(x => x.StatusMonth == dtFrom);
+                    if (st == null)
+                    {
+                        AppLog.WriteLog($"{dtFrom:MMMM yyyy} is missing in History");
+                        return;
+                    }
+                    CMon = CMon + 1;
+                    if (st.TOTAL_MONTHS > 0)
+                    {
+                        PMon = PMon + Convert.ToInt32(st.TOTAL_MONTHS.Value);
+
+                        BF = st.TOTALBF_AMOUNT.Value;
+                        Subs = st.TOTALSUBCRP_AMOUNT.Value;
+                        Ins = st.TOTALINSURANCE_AMOUNT.Value;
+
+                        AccBF += BF;
+                        AccSubs += Subs;
+                        AccIns += Ins;
+
+                        CurBF += BF;
+                        CurSubs += Subs;
+                        CurIns += Ins;
+
+                        st.LASTPAYMENTDATE = dtFrom;
+                        LastPaidDue = 0;
+                    }
+                    else
+                    {
+                        LastPaidDue += 1;
+                    }
+                    ConBF += BF;
+                    ConSubs += Subs;
+                    ConIns += Ins;
+
+                    st.TOTALMONTHSCONTRIBUTION = CMon;
+                    st.TOTALMONTHSPAID = PMon;
+                    st.TOTALMONTHSDUE = CMon - PMon;
+
+                    st.BF_AMOUNT = BF;
+                    st.SUBSCRIPTION_AMOUNT = Subs;
+                    st.INSURANCE_AMOUNT = Ins;
+
+                    st.CURRENT_YDTBF = CurBF;
+                    st.CURRENT_YDTSUBSCRIPTION = CurSubs;
+                    st.CURRENT_YDTINSURANCE = CurIns;
+
+                    st.ACCBF = AccBF;
+                    st.ACCSUBSCRIPTION = AccSubs;
+                    st.ACCINSURANCE = AccIns;
+
+                    st.BFDUE = ConBF-AccBF;
+                    st.SUBSCRIPTIONDUE = ConSubs-AccSubs;
+                    st.INSURANCEDUE = ConIns-AccIns;
+
+                    st.STATUS_CODE = 0;
+                    if (dtResign != null)
+                    {
+                        if(dtResign.Value.Year==dtFrom.Year && dtResign.Value.Month == dtFrom.Month)
+                        {
+                            st.STATUS_CODE = 4;
+                        }
+                    }
+                    if (st.STATUS_CODE == 0)
+                    {
+                        if (LastPaidDue > 12)
+                        {
+                            st.STATUS_CODE = 3;
+                        }
+                        else if (st.TOTALMONTHSDUE > 3)
+                        {
+                            st.STATUS_CODE = 2;
+                        }
+                        else
+                        {
+                            st.STATUS_CODE = 1;
+                        }
+                    }
+                }
+
+
+
+                data.TOTALMONTHSPAID = st.TOTALMONTHSPAID;
+                data.TOTALMONTHSDUE = st.TOTALMONTHSDUE;
+                data.TOTALMONTHCONTRIBUTE = st.TOTALMONTHSCONTRIBUTION;
+
+                data.ACCBF = st.ACCBF;
+                data.ACCSUBSCRIPTION = st.ACCSUBSCRIPTION;
+                data.ACCINSURANCE = st.ACCINSURANCE;
+
+                data.DUEBF = st.BFDUE;
+                data.DUEINSURANCE = st.INSURANCEDUE;
+                data.DUESUBSCRIPTION = st.SUBSCRIPTIONDUE;
+
+                data.CURRENT_YTDBF = st.CURRENT_YDTBF;
+                data.CURRENT_YTDSUBSCRIPTION = st.CURRENT_YDTSUBSCRIPTION;
+                data.CURRENT_YTDINSURANCE = st.CURRENT_YDTINSURANCE;
+
+                data.LASTPAYMENT_DATE = st.LASTPAYMENTDATE;
+
+                data.STATUS_CODE = st.STATUS_CODE;
+
+                db.SaveChanges();
+                MessageBox.Show("Done");
+            }
+            catch(Exception ex)
+            {
+                AppLog.WriteLog(ex);
+                MessageBox.Show(ex.Message);
+            }
+
+            
         }
     }
 }
